1db010d4b58b8753654f8112cdb4b08076d8dbea
omar-abdelgawad <omar.abdelgawad@ejust.edu.eg>
2023-10-09 10:37:33 +0300

    added the first method to the sperm dataclass

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import argparse
 from ultralytics import YOLO
 from dataclasses import dataclass
 from dataclasses import field
+from dataclasses import InitVar
 from collections import defaultdict
 from scipy.fft import rfft, rfftfreq
 from itertools import cycle
@@ -67,13 +68,45 @@ class Sperm:
     """Sperm class for holding and displaying data related to a sperm in a video defined by an id."""
 
     id: int = field()
+    sperm_overlay_image_shape: InitVar[tuple] = field()
+    sperm_overlay_image: np.ndarray = field(default=np.zeros(sperm_overlay_image_shape))
     p_num_5: list[Any] = field(default_factory=list, repr=False)
     p_num_6: list[Any] = field(default_factory=list, repr=False)
     p_num_7: list[Any] = field(default_factory=list, repr=False)
     p_num_8: list[Any] = field(default_factory=list, repr=False)
     head_angle: list[Any] = field(default_factory=list, repr=False)
     sperm_image: Optional[np.ndarray] = field(default=None)
-    # overlay_image: np.ndarray = field(default_factory=np.zeros_like(lstresults[0].orig_img))
+
+    def save_amplitude_figures(self, out_dir: str) -> None:
+        """Creates and saves the amplitude figures of last 4 points of the Sperm.
+
+        Args:
+            out_dir(str): dir to image to.
+
+        Returns:
+            None"""
+        title = f"Signed Amplitude of last 4 points for id {self.id}"
+        xlabel = "frame count"
+        ylabel = "distance between point and head axis in pixels"
+        fig, axes = plt.subplots(2, 2, sharey=True, sharex=True)
+        fig.suptitle(title)
+        fig.text(0.5, 0.04, xlabel, ha="center")
+        fig.text(0.04, 0.5, ylabel, va="center", rotation="vertical")
+        for i in range(2):
+            for j in range(2):
+                axes[i, j].axhline(
+                    y=0, color="black", linestyle="-", linewidth=2, label=None
+                )
+        axes[0, 0].plot(self.p_num_5)
+        axes[0, 0].set_title("point 5")
+        axes[0, 1].plot(self.p_num_6)
+        axes[0, 1].set_title("point 6")
+        axes[1, 0].plot(self.p_num_7)
+        axes[1, 0].set_title("point 7")
+        axes[1, 1].plot(self.p_num_8)
+        axes[1, 1].set_title("point 8")
+        plt.savefig(fname=f"{os.path.join(out_dir,title)}.jpeg")
+        plt.close(fig)
 
 
 def find_fps(video_path: str) -> float:
